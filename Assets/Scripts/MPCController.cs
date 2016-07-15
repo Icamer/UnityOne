b07ebec7d46963173a84de504f1ac0f917d8d965
Icamer <maronicam@gmail.com>
2016-07-15 16:18:12 +0200

    name

---
+++ Assets/Scripts/MPCController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MPCController : MonoBehaviour {
+
+	public float MPCmaxSpeed;
+	bool MPCfacingRight=true;
+	//public Vector2 a;
+	Rigidbody2D MPCRigidBodyOfChar;
+	Animator MPCAnimatorOfChar;
+	// Use this for initialization
+	void Start () {
+		MPCRigidBodyOfChar = GetComponent<Rigidbody2D> ();
+		MPCAnimatorOfChar = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis("Horizontal");
+		MPCAnimatorOfChar.SetFloat("MPCRunning",Mathf.Abs(move));
+		MPCRigidBodyOfChar.velocity=new Vector2(move*MPCmaxSpeed,MPCRigidBodyOfChar.velocity.y);
+		if (move > 0 && !MPCfacingRight)
+			flip ();
+		else if (move < 0 && MPCfacingRight)
+			flip ();
+		
+	}
+	void flip(){
+		Vector3 MPCscale = transform.localScale;
+		MPCscale.x *= -1;
+		transform.localScale = MPCscale;
+		MPCfacingRight = !MPCfacingRight;
+	}
+}
